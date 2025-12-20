a8c7144c4640f571a1431dcdcdb7ed9a76b23157
shabbirkhan <khanshabbir126@gmail.com>
2025-12-20 12:51:22 +0530

    commit

--- src/com/sk/Test.java
+++ src/com/sk/Test.java
@@ -4,6 +4,23 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
+		int arr[] = {1,2,4,6,7};
+		System.out.println("orginal: -" + arr);
+		int i=0;
+		int j=arr.length -1;
+		
+		while(i < j)
+		{
+			int temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			i++;
+			j--;
+			
+		}
+		
+		System.out.println("Reverse:- " + arr);
+		
 	}
 
 }
